2a9b2a3105406e555339493201bd14e31cdc6856
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-06-12 10:20:05 +0200

    feat: add Buy me a coffee to sponsor funnel (navbar icons + Support menu + footer CTA)

--- components/MantineNavBar/MantineNavBar.tsx
+++ components/MantineNavBar/MantineNavBar.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { Navbar } from 'nextra-theme-docs';
-import { Button, Group, Text } from '@mantine/core';
-import { IconHeartFilled } from '@tabler/icons-react';
+import { ActionIcon, Group, Text, Tooltip } from '@mantine/core';
+import { IconCoffee, IconHeartFilled } from '@tabler/icons-react';
 import { AmigaModeToggle } from '../AmigaModeToggle/AmigaModeToggle';
 import { ColorSchemeControl } from '../ColorSchemeControl/ColorSchemeControl';
 import { Logo } from '../Logo/Logo';
@@ -24,19 +24,39 @@ export const MantineNavBar = () => {
         chatLink="https://eab.abime.net/"
         projectLink="https://github.com/gfazioli/amiga-assembly-library"
       >
-        <AmigaModeToggle />
-        <ColorSchemeControl />
-        <Button
-          component="a"
-          href="#sponsors"
-          size="sm"
-          radius="xl"
-          variant="gradient"
-          gradient={{ from: 'pink', to: 'grape' }}
-          leftSection={<IconHeartFilled size={14} />}
-        >
-          Sponsor
-        </Button>
+        <Group gap="sm" wrap="nowrap">
+          <AmigaModeToggle />
+          <ColorSchemeControl />
+          <Tooltip label="Sponsor" withArrow>
+            <ActionIcon
+              component="a"
+              href="#sponsors"
+              size="lg"
+              radius="xl"
+              variant="gradient"
+              gradient={{ from: 'pink', to: 'grape' }}
+              aria-label="Sponsor"
+            >
+              <IconHeartFilled size={16} />
+            </ActionIcon>
+          </Tooltip>
+          <Tooltip label="Buy me a coffee" withArrow>
+            <ActionIcon
+              component="a"
+              href="https://donate.stripe.com/fZu4gy4Tn3b1dgudGx0co00"
+              target="_blank"
+              rel="noopener noreferrer"
+              size="lg"
+              radius="xl"
+              variant="filled"
+              color="yellow"
+              aria-label="Buy me a coffee"
+              styles={{ root: { color: 'var(--mantine-color-white)' } }}
+            >
+              <IconCoffee size={16} />
+            </ActionIcon>
+          </Tooltip>
+        </Group>
       </Navbar>
     </>
   );
